86482e7fe5f440323cc4376ec26d009dce948386
sergeiioannisyan <sergeicoding@gmail.com>
2022-01-29 00:05:07 -0500

    add first tests add mocha config

---
+++ specs/example.spec.js
@@ -0,0 +1,14 @@
+import { expect } from "chai"
+
+describe( 'Math functions', function() {
+    it( 'A + B + C', function () {
+      const a = 4
+      const b = 5
+      const c = a + b
+      expect(c).to.eq( 9)
+    })
+
+    it('A - B', function () {
+
+    })
+})
