7be30207c5a9eb7a184314a99ef3cf82632f4148
Sergeicoding <66889666+Sergeicoding@users.noreply.github.com>
2022-02-04 23:12:40 -0500

    add environment variables support (#3)

---
+++ specs/auth.spec.js
@@ -0,0 +1,27 @@
+import supertest from 'supertest'
+import { expect } from 'chai'
+import 'dotenv/config'
+
+describe('Auth', function (){
+    const request = supertest(process.env.BASE_URL)
+
+    it('Successful log in', function (){
+       request
+           .post('/auth')
+           .send({login: process.env.LOGIN, password: process.env.PASSWORD })
+           .end(function (err, res){
+               expect(res.statusCode).to.eq(200)
+               expect(res.body.token).not.to.be.undefined
+           })
+    })
+
+    it('Login in with invalid credentials', function (){
+        request
+            .post('/auth')
+            .send({login: 'invalid', password: 'invalid'})
+            .end(function (err, res){
+                expect(res.statusCode).to.eq(404)
+                expect(res.body.message).to.eq('Wrong login or password.')
+            })
+    })
+})
